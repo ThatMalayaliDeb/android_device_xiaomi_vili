4e7c74c26c95e01a6b13544c0cd03220fdb172b5
ThatMalayaliDeb <theoverpoweredguy@gmail.com>
2024-01-03 13:44:50 +0530

    vendorsetup.sh: Update repo sources

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,21 +11,21 @@ git clone --depth=1 https://github.com/ProjectElixir-Devices/android_vendor_xiao
 echo 'Cloning Kernel tree [3/5]'
 # Kernel Tree
 rm -rf kernel/xiaomi/sm8350
-git clone --depth=1 git@github.com:ThatMalayaliDeb/kernel_xiaomi_sm8350lineage.git -b lineage_14 kernel/xiaomi/sm8350
+git clone --depth=1 https://github.com/ThatMalayaliDeb/kernel_xiaomi_sm8350lineage.git -b lineage_14 kernel/xiaomi/sm8350
 
 echo 'Cloning Hardware xiaomi [4/5]'
 # Hardware xiaomi
 rm -rf hardware/xiaomi
-git clone --depth=1 git@github.com:ThatMalayaliDeb/hardware_xiaomi.git -b evo hardware/xiaomi
+git clone --depth=1 https://github.com/ThatMalayaliDeb/hardware_xiaomi.git -b evo hardware/xiaomi
 
 echo 'Cloning Leica Cam [5/5]'
-# Op Cam
+# Leica Cam
 rm -rf vendor/xiaomi/vili-miuicamera
-git clone --depth=1 git@gitlab.com:ThatMalayaliDeb/vili-leicacamera.git vendor/xiaomi/vili-miuicamera
+git clone --depth=1 https://gitlab.com/ThatMalayaliDeb/vili-leicacamera.git vendor/xiaomi/vili-miuicamera
 
 echo 'Cloning vili Firmware'
 # vili firmware
 rm -rf vendor/xiaomi/vili-firmware
-git clone --depth=1 git@gitlab.com:ThatMalayaliDeb/vendor_xiaomi_vili-firmware.git -b 13.0 vendor/xiaomi/vili-firmware
+git clone --depth=1 https://gitlab.com/ThatMalayaliDeb/vendor_xiaomi_vili-firmware.git -b 13.0 vendor/xiaomi/vili-firmware
 
 echo 'Completed, Now proceeding to lunch'
