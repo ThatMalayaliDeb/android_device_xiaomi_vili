2bd71278a25323ed0e4956c0865aa83655ac94b7
Nandu VK <67923641+ThatMalayaliDeb@users.noreply.github.com>
2024-01-22 00:01:23 +0530

    vili: Patch framework/native for Leica Camera

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,9 @@
+# Leica
+echo 'Adding Leica Camera patches'
+cd frameworks/native
+wget https://raw.githubusercontent.com/ThatMalayaliDeb/vili_patches/master/NativeProducerListenerLeicaPatch.patch
+patch -p1 <NativeProducerListenerLeicaPatch.patch
+cd ../..
+
+echo 'End of Patches'
 
