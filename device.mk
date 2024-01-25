c3d305f9e053e45423871ba4b260dfcffc978d82
ThatMalayaliDeb <theoverpoweredguy@gmail.com>
2024-01-25 09:12:44 +0000

    vili: Add back Lineage FastCharge implementation !
    This reverts commit 216437c4ad64654669a44b066b19094c3e223bd8.

--- device.mk
+++ device.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.vili
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi_vili \
