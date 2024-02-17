c38946145f95f2e452e818141d305c3e36cb4f7c
ThatMalayaliDeb <theoverpoweredguy@gmail.com>
2024-02-17 19:50:14 +0000

    vili: vendorsetup: Tracking custom Display HAL

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,8 @@
 echo "Cloning Xiaomi hardware"
 rm -rf hardware/xiaomi
 git clone https://github.com/ThatMalayaliDeb/hardware_xiaomi.git -b evo hardware/xiaomi
+
+# Cloning Display HALs
+echo 'Cloning Display HALs'
+rm -rf hardware/qcom-caf/sm8350/display
+git clone https://github.com/ThatMalayaliDeb/android_hardware_qcom_display.git -b 14 hardware/qcom-caf/sm8350/display
